ac651b3af6c2c65c11f31c4f46c3f2528a49443b
bahayg <bahayg@gmail.com>
2020-02-25 10:04:09 -0800

    Filter charities  by city

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,2 +1,18 @@
 class CharitiesController < ApplicationController
-end
+    def index
+        @charities = Charity.all
+        render json: @charities
+    end
+
+    # def show 
+    #     @charity = Charity.find(params[:id])
+    #     render json: @charity
+    # end
+
+    private
+
+    def charity_params
+        params.require(:charity).permit(:user_id, :name, :image, :address, :city)
+    end
+
+end 
